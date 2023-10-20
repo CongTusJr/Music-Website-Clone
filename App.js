6047ff29cd6b5c959b5c60e57c794430eaf87edf
CongTusJr <congtutran2k2@gmail.com>
2023-10-20 22:27:53 +0700

    1

--- App.js
+++ App.js
@@ -166,7 +166,101 @@ Array.from(document.getElementsByClassName('playListPlay')).forEach((e)=>{
         makeAllplays();
         el.target.classList.remove('bi-play-circle-fill');
         el.target.classList.add('bi-pause-circle-fill');
-    })
+        wave.classList.remove('active1');
+    });
+})
+
+let currentStart = document.getElementById('currentStart');
+let currentEnd = document.getElementById('currentEnd');
+let seek = document.getElementById('seek');
+let bar2 = document.getElementById('bar2');
+let dot = document.getElementsByClassName('dot')[0];
+music.addEventListener('timeupdate', ()=>{
+    let music_curr = music.currentTime;
+    let music_dur = music.duration;
+    // console.log(music_dur);
+    let min1 = Math.floor(music_dur / 60);
+    let sec1 = Math.floor(music_dur % 60);
+    // console.log(min1);
+    if(sec1<10){
+        sec1=`0${sec1}`;
+    }
+    currentEnd.innerText=`${min1}:${sec1}`;
+
+    let min2 = Math.floor(music_curr/60);
+    let sec2 = Math.floor(music_curr%60);
+    if(sec2<10){
+        sec2=`0${sec2}`;
+    }
+    currentStart.innerText=`${min2}:${sec2}`;
+
+    let progressBar = parseInt((music_curr / music_dur)*100);
+    seek.value=progressBar;
+    // console.log(seek.value);
+    let seekbar = seek.value;
+    bar2.style.width=`${seekbar}%`;
+    dot.style.left =`${seekbar}%`;
+});
+
+seek.addEventListener('change',()=>{
+    music.currentTime=seek.value * music.duration/100;
+});
+
+let vol_icon= document.getElementById('vol_icon');
+let vol = document.getElementById('vol');
+let vol_bar =document.getElementById('vol_bar')[0];
+let vol_dot =document.getElementById('vol_dot');
+
+vol.addEventListener('change', ()=>{
+    if(vol.value ==0){
+        vol_icon.classList.remove('bi-volume-up-fill');
+        vol_icon.classList.remove('bi-volume-down-fill');
+        vol_icon.classList.add('bi-volume-mute-fill');
+    }
+    if(vol.value>0){
+        vol_icon.classList.remove('bi-volume-up-fill');
+        vol_icon.classList.add('bi-volume-down-fill');
+        vol_icon.classList.remove('bi-volume-mute-fill');
+    }
+    if(vol.value>50){
+        vol_icon.classList.add('bi-volume-up-fill');
+        vol_icon.classList.remove('bi-volume-down-fill');
+        vol_icon.classList.remove('bi-volume-mute-fill');
+    }
+    let vol_a = vol.value;
+    vol_bar.style.width = `${vol_a}%`;
+    vol_dot.style.left = `${vol_a}%`;
+    music.volume = vol_a/100;
+
+});
+
+let back = document.getElementById('back');
+let next =  document.getElementById('next');
+
+back.addEventListener('click',()=>{
+    index -=1;
+    music.src=`Audio/Sơn Tùng M-TP/${index}.mp3`;
+        poster_master_play.src=`Image/Sơn Tùng M-TP/${index}.jpg`;
+        music.play();
+
+        masterPlay.classList.add('bi-play-fill');
+        masterPlay.classList.remove('bi-pause-fill');
+        document
+            .getElementById('masterPlay')
+            .classList.replace('bi-play-fill', 'bi-pause-fill');
+        let songTitles = songs.filter((els)=>{
+            return els.id == index;
+        });
+        songTitles.forEach(elss =>{
+            let {songname}=elss;
+            title.innerHTML = songname;
+        });
+        makeAllBackground();
+        Array.from(document.getElementsByClassName('songItem'))[index-1].style.background="rgb(105, 105, 105, .1)";
+        makeAllplays();
+        el.target.classList.remove('bi-play-circle-fill');
+        el.target.classList.add('bi-pause-circle-fill');
+        wave.classList.remove('active1');
 })
 
 let pop_song_left = document.getElementById('pop_song_left');
